6c01349506570b363ba995e5b371b541725c23b4
ismail debbarh <usamdh07@gmail.com>
2023-10-27 16:34:07 +0100

    refactor: Change validation code

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,74 +1,51 @@
-export function isValidForm(errors) {
-  for (let value of Object.values(errors)) {
-    if (value.error === true) {
-      return false;
-    }
-  }
-  return true;
-}
+const validateFieldHandler = {
+  require: (fieldValue, validatorValue) =>
+    !validatorValue || fieldValue.length > 0,
+  maxLength: (fieldValue, validatorValue) =>
+    fieldValue.length <= validatorValue,
+  minLength: (fieldValue, validatorValue) =>
+    fieldValue.length >= validatorValue,
+  max: (fieldValue, validatorValue) => parseFloat(fieldValue) <= validatorValue,
+  min: (fieldValue, validatorValue) => parseFloat(fieldValue) >= validatorValue,
+  custom: (field, validatorValue) => validatorValue(field),
+};
 
-export function isValideValidator(field, validator) {
-  let isValid = true;
-  switch (validator[0]) {
-    case "require":
-      isValid = typeof validator[1] === "boolean";
-      break;
-    case "maxLength":
-      isValid = typeof validator[1] === "number";
-      break;
-    case "minLength":
-      isValid = typeof validator[1] === "number";
-      break;
-    case "max":
-      isValid = typeof validator[1] === "number" && field.type === "number";
-      break;
-    case "min":
-      isValid = typeof validator[1] === "number" && field.type === "number";
-      break;
-    case "custom":
-      isValid =
-        typeof validator[1] === "function" &&
-        typeof validator[1](field.value) === "boolean";
-      break;
-    default:
-      throw Error(
-        `Error: ${validator[0]} is not a valid validator name. Please provide a valid one.`,
-      );
-  }
-  return isValid;
+const validateValidatorHandler = {
+  require: (_, validatorValue) => typeof validatorValue === "boolean",
+  maxLength: (_, validatorValue) => typeof validatorValue === "number",
+  minLength: (_, validatorValue) => typeof validatorValue === "number",
+  max: (field, validatorValue) =>
+    typeof validatorValue === "number" &&
+    ["number", "range"].includes(field.type),
+  max: (field, validatorValue) =>
+    typeof validatorValue === "number" &&
+    ["number", "range"].includes(field.type),
+  custom: (field, validatorValue) =>
+    typeof validatorValue === "function" &&
+    typeof validatorValue(field.value) === "boolean",
+};
+
+export function isValidForm(errors) {
+  return Object.values(errors).some((value) => value.error);
 }
 
 export function validateField(field, validator) {
-  if (!isValideValidator(field, validator)) {
+  const validatorType = validator[0];
+  const validatorValue = validator[1];
+
+  if (!validateValidatorHandler.hasOwnProperty(validatorType)) {
     throw Error(
-      `Error: The validation object you provide is not a valid . Please provide a valid one.`,
+      `Error: ${validatorType.toUpperCase()} is not a valid validator type.`,
     );
   }
 
-  let isValid = true;
-  switch (validator[0]) {
-    case "require":
-      isValid = field.value.length > 0;
-      break;
-    case "maxLength":
-      isValid = field.value.length <= validator[1];
-      break;
-    case "minLength":
-      isValid = field.value.length >= validator[1];
-      break;
-    case "max":
-      isValid =
-        parseInt(field.value.length > 0 ? field.value : "0") <= validator[1];
-      break;
-    case "min":
-      isValid =
-        parseInt(field.value.length > 0 ? field.value : "0") >= validator[1];
-      break;
-    case "custom":
-      isValid = validator[1](field.value);
-      break;
-    default:
-      break;
+  const validateValidatorFunction = validateValidatorHandler[validatorType];
+
+  if (!validateValidatorFunction(field, validatorValue)) {
+    throw Error(`Error: The validation object you provide is not a valid.`);
   }
-  return isValid;
+
+  const validateFieldFunction = validateFieldHandler[validator[0]];
+
+  return validateFieldFunction(field.value, validator[1]);
 }
